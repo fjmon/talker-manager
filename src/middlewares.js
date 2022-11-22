a3723a848d5c2f2d0f7e763d100b435d6f7c7361
Fernando Monteiro <fjmonteiro@gmail.com>
2022-11-22 20:17:59 -0300

    req 4 confiante com ajustes

--- src/middlewares.js
+++ src/middlewares.js
@@ -8,23 +8,19 @@ function validacao(req, res, next) {
     const emailVal = /\S+@\S+\.\S+/;
 
     if (!email) {
-        return res.status(400)
-        .json({ message: 'O campo "email" é obrigatório' });
+        return res.status(400).json({ message: 'O campo "email" é obrigatório' });
     }
     if (!emailVal.test(email)) {
-        return res.status(400)
-        .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+        return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
     }
     if (!password) {
-        return res.status(400)
-        .json({ message: 'O campo "password" é obrigatório' });
+        return res.status(400).json({ message: 'O campo "password" é obrigatório' });
     }
     if (password.length < 6) {
-        return res.status(400)
-        .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+        return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
     }
 
     return next();
-};
+}
 
 module.exports = { generateToken, validacao };
